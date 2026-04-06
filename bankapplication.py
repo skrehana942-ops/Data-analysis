8a3c046a28e7b8148b26515366c15735efee4630
skrehana942-ops <skrehana942@gmail.com>
2026-04-06 17:45:29 +0530

    Update bankapplication.py

--- bankapplication.py
+++ bankapplication.py
@@ -1,9 +1,1 @@
-class student:
-    def __init__(self,name,age):
-        self.name=name
-        self.age=age
-    def display(self):
-        print("name:",self.name)
-        print("age:",self.age)
-s1=student("rahul",22)
-s1.display()
+
